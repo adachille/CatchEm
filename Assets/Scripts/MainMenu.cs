767fb0bd21a5aa379e2477e828d51c3311903b0f
adachille <adachille15@gmail.com>
2024-10-07 08:54:25 -0400

    Add quit button

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,20 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync("TestZone");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
